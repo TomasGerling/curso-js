64200498dd5c7207dcc06e3d88283f056070ac92
TomasGerling <tomasgerling@hotmail.com>
2022-06-26 21:49:53 -0300

    Esto es una prueba

--- index.js
+++ index.js
@@ -1,6 +1,6 @@
 function calcular_precio (cantidad_producto, precio_producto){
     if ( cantidad_producto >= 1){
-        console.log (`usted debe pagar: ${precio_producto * cantidad_producto} ¡el envio es gratis! `)
+        console.log (`usted debe pagar: $${precio_producto * cantidad_producto} ¡el envio es gratis! `)
     }
     else if( cantidad_producto <= 0){
         console.log ("usted selecciono una cantida erronea")
@@ -66,21 +66,13 @@ let lista_alimentos = [
 // Para agregar un nuevo producto al array
 for( let i = 0; i < 1 ; i++){
     let product_id = parseInt(prompt("Ingrese el id del producto"));
-
     let nombre_alimento = prompt("Ingrese el nombre del producto");
-
     let cantidad_producto = parseInt(prompt("Ingrese cuantas bolsas tiene disponible *En numeros*"));
-
     let peso_por_bolsa = parseFloat(prompt("Ingrese cuanto pesa cada bolsa *Solo el numero*"));
-
     let tipo_mascota = prompt("Ingrese el tipo de mascota *Perro, Gato u Otro*");
-
     let precio_bolsa = parseInt(prompt("Ingrese el precio de venta por Bolsa *Solo el numero*"));
-
     let precio_kg = parseInt(prompt("Ingrese el precio de venta por KG*Solo el numero*"));
-
     let imagen_bolsa = prompt("Ingrese el url de la imagen de la bolsa");
-
     let alimento_nuevo = new Alimentos(product_id, nombre_alimento , cantidad_producto , peso_por_bolsa, tipo_mascota, precio_bolsa, precio_kg, imagen_bolsa);
 
     lista_alimentos.push(alimento_nuevo);
@@ -94,13 +86,9 @@ console.log("Bienvenide a Peluditos Petshop");
 
 function ingresar_datos(){
     let nombre_cliente = prompt("Ingrese su nombre completo");
-
     let direccion_cliente = prompt("Ingrese su direccion para el envio o si retira en el local");
-    
     let producto_cliente = prompt("Ingrese que alimento desea comprar");
-    
     let precio_producto = prompt("Ingrese el valor por KG del producto");
-    
     let cantidad_producto = prompt("Ingrese cuanto quiere comprar (numero)");
     return [nombre_cliente, direccion_cliente, producto_cliente, precio_producto, cantidad_producto]
 }
@@ -113,3 +101,4 @@ for (let i = 0; i < datos.length; i++){
 // console.log("Su direccion es: " + direccion_cliente);
 // console.log("Usted selecciono que quiere comprar: " + producto_cliente);
 calcular_precio(datos[4], datos[3])
+
